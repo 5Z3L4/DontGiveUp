2ee5a9736e4de249e99db48e69af74138d1c6e0d
5Z3L4 <xqonam@gmail.com>
2020-10-03 12:28:12 +0200

    Movement
    Player Movement added.

---
+++ DGU/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //move variables
+    private float horizontalAxis;
+    public float moveSpeed;
+    public bool facingRight = true;
+
+    //jump variables
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public float jumpForce = 5;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    private bool isGrounded;
+    private bool isJumping;
+    private bool isJumpingLow;
+
+    public Rigidbody2D playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAxis();   
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        Move(horizontalAxis, playerRB, moveSpeed);
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (isJumping && isGrounded)
+        {
+            playerRB.velocity = Vector2.up * jumpForce;
+        }
+        if (playerRB.velocity.y < 0)
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (playerRB.velocity.y > 0 && !isJumpingLow)
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void CheckAxis()
+    {
+        horizontalAxis = Input.GetAxisRaw("Horizontal");
+        isJumping = Input.GetButtonDown("Jump");
+        isJumpingLow = Input.GetButton("Jump");
+    }
+
+    public void Move(float horizontal, Rigidbody2D rb, float speed)
+    {
+        rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, rb.velocity.y);
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    public void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+}
