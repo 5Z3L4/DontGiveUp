26c5e38ae936f853d3f2fde40fae4e1e4191b6fd
5Z3L4 <xqonam@gmail.com>
2020-10-05 01:12:03 +0200

    Merge

---
+++ DGU/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    public void ResumeButton()
+    {
+        Resume();
+    }
+}
