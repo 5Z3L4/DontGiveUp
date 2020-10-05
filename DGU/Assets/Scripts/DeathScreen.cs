3656cd35bb24d83606237437a01890f03f1377b5
OsheOse <osheose0117@gmail.com>
2020-10-05 22:34:31 +0200

    death_screen_fix

---
+++ DGU/Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScreen : MonoBehaviour
+{
+    public GameObject deathScreen;
+    private PlayerStats playerStats;
+    public bool isDead;
+    public float delayTime = 0.3f;
+    private void Awake()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+    }
+    void Update()
+    {
+        isDead = playerStats.isDead;
+        if(isDead)
+        {
+            deathScreen.SetActive(true);
+            Cursor.visible = true;
+        }
+        else
+        {
+            deathScreen.SetActive(false);
+            Cursor.visible = false;
+        }
+    }
+}
